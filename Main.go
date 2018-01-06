5503c1ac2399a15da5b4931658106cc93d108ea1
changlz <changlizhi@aliyun.com>
2018-01-06 20:28:15 +0800

    获取sn

--- Main.go
+++ Main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"github.com/gin-gonic/gin"
 	"io"
 	"log"
@@ -29,45 +28,13 @@ func main() {
 	gin.DefaultWriter = io.MultiWriter(f)
 	r := gin.Default()
 	r.Use(Cors())
-	lingpai := r.Group("jwtlingpai")
+	sn := r.Group("sn")
 	{
-		lingpai.GET("/shengcheng/:xinxis", func(c *gin.Context) {
+		sn.GET("/jieshou", func(c *gin.Context) {
+			log.Println(gongjus.Time2stringnyrsfm(time.Now()))
 			fh := Fanhui{}
-			xinxis := c.Param("xinxis")
-			jx := &gongjus.Xinxis{}
-			err := json.Unmarshal([]byte(xinxis), jx)
-			if err != nil {
-				fh.Zhi = err.Error()
-				fh.Zhuangtai = "jiexishibai"
-				c.JSON(http.StatusOK, fh)
-				return
-			}
-			lpstr, err := gongjus.Shengchenglingpai(jx)
-			if err != nil {
-				fh.Zhi = err.Error()
-				fh.Zhuangtai = "shengchengshibai"
-				c.JSON(http.StatusOK, fh)
-				return
-			}
+			lpstr := "clztest"
 			fh.Zhi = lpstr
-			fh.Zhuangtai = "chenggong"
-			c.JSON(http.StatusOK, fh)
-			return
-		})
-		lingpai.GET("/yanzheng/:xinxis", func(c *gin.Context) {
-			xinxis := c.Param("xinxis")
-			log.Println("xinxis-----", xinxis)
-			lingpai := c.GetHeader("Authorization")
-			fh := Fanhui{}
-			clis, err := gongjus.Yanzhenglingpai(lingpai)
-			if err != nil {
-				fh.Zhi = err.Error()
-				fh.Zhuangtai = "yanzhengshibai"
-				c.JSON(http.StatusOK, fh)
-				return
-			}
-			log.Println("clis-----", clis)
-			fh.Zhi = "/zhanghao1s/yemian"
 			fh.Zhuangtai = "chenggong"
 			c.JSON(http.StatusOK, fh)
 			return
