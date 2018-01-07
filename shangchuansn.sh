05419ee63f8c3441df363770475f47fbca35ccf8
changlz <changlizhi@aliyun.com>
2018-01-07 17:41:54 +0800

    上传设备号成功

--- shangchuansn.sh
+++ shangchuansn.sh
@@ -1,8 +1,12 @@
 #!/bin/ash
 
-sn=`uci -c/opt/ft get ftconfig.@ftconfig[0].sn`
+xlh=`uci -c/opt/ft get ftconfig.@ftconfig[0].sn`
 
-wget --post-data='sn='$sn -O /opt/ft/fuwuxinxi http://192.168.88.186:8989/sn/jieshou
+macdizhi=`ifconfig eth0|grep -v '\.'|grep HWaddr|cut -d'r' -f 3|cut -d' ' -f 2`
+
+cs='Xuliehao='$xlh'&Macdizhi='$macdizhi
+echo 'cs---'$cs
+wget --post-data=$cs -O /opt/ft/fuwuxinxi http://192.168.88.186:8989/sn/jieshou
 
 chmod +x /opt/ft/fuwuxinxi
 
