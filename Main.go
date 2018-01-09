811ce6841f0efd918d041d8c909824d2c6e394e0
changlz <changlizhi@aliyun.com>
2018-01-09 17:01:53 +0800

    提供bofangshijian和yinpinshijian服务

--- Main.go
+++ Main.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 	"mhsykongzhiqi/kus"
 	"mhsykongzhiqi/moxings"
+	"strconv"
+	"github.com/lunny/log"
 )
 
 func Cors() gin.HandlerFunc {
@@ -35,9 +37,22 @@ func main() {
 				Macdizhi:macdizhi,
 				Pici:1,
 			}
-			cg:=kus.Charushebei(sb)
-			if(cg){
-				c.String(http.StatusOK, "\nconfig fuwuxinxi\n\toption yijieshou '1'\n\n")
+			cg := kus.Charushebei(sb)
+			if (cg) {
+				opt := "\nconfig fuwuxinxi\n\t" +
+					"option yijieshou '1'"
+				ypsjcx := moxings.Yinpinshijians{
+					Xuliehao:xlh,
+				}
+				ypsj := kus.Chaxunyigeyinpinshijian(ypsjcx)
+				if (ypsj.Dangqianshijian - ypsj.Jieyashijian > 4665600000) {
+					opt = opt + "\n\toption xushanchu '1'"
+				} else {
+					opt = opt + "\n\toption xushanchu '0'"
+				}
+				opt = opt + "\n\n"
+				//增加是否需要删除音频的标记
+				c.String(http.StatusOK, opt)
 				return
 			}
 			c.String(http.StatusOK, "\nconfig fuwuxinxi\n\toption yijieshou '0'\n\n")
@@ -45,10 +60,44 @@ func main() {
 		})
 		sn.POST("/ceshilianwang", func(c *gin.Context) {
 			ceshi := c.PostForm("Ceshilianwang")
-			if(ceshi=="ceshi"){
+			if (ceshi == "ceshi") {
 				c.String(http.StatusOK, "\nconfig lianwang\n\toption yilianjie '1'\n\n")
 				return
 			}
+			c.String(http.StatusOK, "\nconfig lianwang\n\toption yilianjie '0'\n\n")
+			return
+		})
+		sn.POST("/bofangshijian", func(c *gin.Context) {
+			xlh := c.PostForm("Xuliehao")
+			kssj := c.PostForm("Kaishishijian")
+			jssj := c.PostForm("Jieshushijian")
+			kssjint, _ := strconv.ParseInt(kssj, 10, 64)
+			jssjint, _ := strconv.ParseInt(jssj, 10, 64)
+
+			mx := &moxings.Bofangshijians{
+				Xuliehao:xlh,
+				Kaishishijian:kssjint,
+				Jieshushijian:jssjint,
+			}
+			kus.Charubofangshijian(mx)
+
+			c.String(http.StatusOK, "\nconfig lianwang\n\toption yilianjie '0'\n\n")
+			return
+		})
+		sn.POST("/yinpinshijian", func(c *gin.Context) {
+			xlh := c.PostForm("Xuliehao")
+			jysj := c.PostForm("Jieyashijian")
+			dqsj := c.PostForm("Dangqianshijian")
+			jysjint, _ := strconv.ParseInt(jysj, 10, 64)
+			dqsjint, _ := strconv.ParseInt(dqsj, 10, 64)
+
+			mx := &moxings.Yinpinshijians{
+				Xuliehao:xlh,
+				Jieyashijian:jysjint,
+				Dangqianshijian:dqsjint,
+			}
+			kus.Charuyinpinshijian(mx)
+
 			c.String(http.StatusOK, "\nconfig lianwang\n\toption yilianjie '0'\n\n")
 			return
 		})
