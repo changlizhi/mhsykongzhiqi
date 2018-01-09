6e071012addb2380f983dc9569842c099a580fbe
changlz <changlizhi@aliyun.com>
2018-01-09 17:01:18 +0800

    把timestamp改为int64

--- moxings/Bofangshijians.go
+++ moxings/Bofangshijians.go
@@ -5,8 +5,8 @@ import "time"
 type Bofangshijians struct {
 	Id            int
 	Xuliehao      string    `gorm:"not null;DEFAULT:0"`
-	Kaishishijian time.Time `gorm:"not null;DEFAULT:'1970-01-01 10:00:00'"`
-	Jieshushijian time.Time `gorm:"not null;DEFAULT:'1970-01-01 10:00:00'"`
+	Kaishishijian int64  `gorm:"not null;DEFAULT:0"`
+	Jieshushijian int64  `gorm:"not null;DEFAULT:0"`
 }
 
 func (Bofangshijians) TableName() string {
--- moxings/Yinpinshijians.go
+++ moxings/Yinpinshijians.go
@@ -5,8 +5,8 @@ import "time"
 type Yinpinshijians struct {
 	Id              int
 	Xuliehao        string    `gorm:"not null;DEFAULT:0"`
-	Jieyashijian    time.Time `gorm:"not null;DEFAULT:'1970-01-01 10:00:00'"`
-	Dangqianshijian time.Time `gorm:"not null;DEFAULT:'1970-01-01 10:00:00'"`
+	Jieyashijian    int64  `gorm:"not null;DEFAULT:0"`
+	Dangqianshijian int64  `gorm:"not null;DEFAULT:0"`
 }
 
 func (Yinpinshijians) TableName() string {
