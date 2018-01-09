fff3c1fb4f0e8505cd5dc337fae45f9a32fba5ad
changlz <changlizhi@aliyun.com>
2018-01-09 17:01:30 +0800

    增加插入操作

--- kus/Bofangshijiankus.go
+++ kus/Bofangshijiankus.go
@@ -4,13 +4,23 @@ import (
 	"log"
 	"mhsykongzhiqi/moxings"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
 )
 
-func Charuyinpinshijian(moxing *moxings.Yinpinshijians) bool {
+func Charubofangshijian(moxing *moxings.Bofangshijians) bool {
 	cr := Db.Create(moxing)
 	if cr.Error != nil {
-		log.Println("Charuyinpinshijian----cr.Error---", cr.Error)
+		log.Println("Charubofangshijian----cr.Error---", cr.Error)
 		return false
 	}
 	return true
+}
+func Chaxunyigebofangshijian(moxing moxings.Bofangshijians) *moxings.Bofangshijians {
+	find := Db.Find(&moxings.Bofangshijians{}, moxing)
+	if find.Error != nil && find.Error == gorm.ErrRecordNotFound {
+		log.Println("Chaxunyigebofangshijian--find.Error---", find.Error)
+		return nil
+	}
+	ret := find.Value.(*moxings.Bofangshijians)
+	return ret
 }
--- kus/Yinpinshijiankus.go
+++ kus/Yinpinshijiankus.go
@@ -4,13 +4,23 @@ import (
 	"log"
 	"mhsykongzhiqi/moxings"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
 )
 
-func Charubofangshijian(moxing *moxings.Bofangshijians) bool {
+func Charuyinpinshijian(moxing *moxings.Yinpinshijians) bool {
 	cr := Db.Create(moxing)
 	if cr.Error != nil {
-		log.Println("Charubofangshijian----cr.Error---", cr.Error)
+		log.Println("Yinpinshijians----cr.Error---", cr.Error)
 		return false
 	}
 	return true
+}
+func Chaxunyigeyinpinshijian(moxing moxings.Yinpinshijians) *moxings.Yinpinshijians {
+	find := Db.Find(&moxings.Yinpinshijians{}, moxing)
+	if find.Error != nil && find.Error == gorm.ErrRecordNotFound {
+		log.Println("Chaxunyigeyinpinshijian--find.Error---", find.Error)
+		return nil
+	}
+	ret := find.Value.(*moxings.Yinpinshijians)
+	return ret
 }
