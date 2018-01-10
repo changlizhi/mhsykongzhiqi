38d195a46e22da2bc181665ec0c865b0600619d2
changlz <changlizhi@aliyun.com>
2018-01-10 09:54:03 +0800

    增加创建表的功能

--- kus/Aquanju.go
+++ kus/Aquanju.go
@@ -1,5 +1,14 @@
 package kus
 
-import "github.com/jinzhu/gorm"
+import (
+	"github.com/jinzhu/gorm"
+	"mhsykongzhiqi/moxings"
+)
 
 var Db, _ = gorm.Open("mysql", "root:root@tcp(127.0.0.1:3306)/mhsyshuju?charset=utf8&parseTime=True&loc=Local")
+
+func Chuangjianbiao() {
+	Db.Set("gorm:table_options", "ENGINE=InnoDB").CreateTable(
+		&moxings.Yinpinwenjians{},
+	)
+}
---
+++ tests/Aquanju_test.go
@@ -0,0 +1,10 @@
+package tests
+
+import (
+	"testing"
+	"mhsykongzhiqi/kus"
+)
+
+func TestChuangjianbiao(t *testing.T) {
+	kus.Chuangjianbiao()
+}
