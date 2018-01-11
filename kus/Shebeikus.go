f43b0a4c8028d2af92e842107e9ab64fa1c15308
changlz <changlizhi@aliyun.com>
2018-01-11 15:41:07 +0800

    增加查询设备号的方法，已存在就不再存了

--- kus/Shebeikus.go
+++ kus/Shebeikus.go
@@ -4,7 +4,20 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"log"
 	"mhsykongzhiqi/moxings"
+	"github.com/jinzhu/gorm"
 )
+
+
+func Chaxunyigeshebei(moxing moxings.Shebeis) *moxings.Shebeis {
+	find := Db.Find(&moxings.Shebeis{}, moxing)
+	if find.Error != nil && find.Error == gorm.ErrRecordNotFound {
+		log.Println("Chaxunyigeshebei--find.Error---", find.Error)
+		return nil
+	}
+	ret := find.Value.(*moxings.Shebeis)
+	return ret
+}
+
 
 func Charushebei(moxing *moxings.Shebeis) bool {
 	cr := Db.Create(moxing)
