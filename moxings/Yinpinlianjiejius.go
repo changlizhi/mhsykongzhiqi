1bc6cdadc9946d93b5d29e1d136002e4d1ed2543
changlz <changlizhi@aliyun.com>
2018-01-11 22:21:42 +0800

    默认值用单引号表示字符串

--- moxings/Yinpinlianjiejius.go
+++ moxings/Yinpinlianjiejius.go
@@ -2,8 +2,8 @@ package moxings
 
 type Yinpinlianjiejius struct {
 	Id              int
-	Xuliehao        string `gorm:"not null;DEFAULT:0"`
-	Lianjie        string `gorm:"not null;DEFAULT:0"`
+	Xuliehao        string `gorm:"not null;DEFAULT:'0'"`
+	Lianjie        string `gorm:"not null;DEFAULT:'0'"`
 }
 
 func (Yinpinlianjiejius) TableName() string {
