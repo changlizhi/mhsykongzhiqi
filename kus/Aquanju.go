44df35f0b4631a0bee4281ae9d336819ec8bc84d
changlz <changlizhi@aliyun.com>
2018-01-11 15:52:54 +0800

    创建表时打印错误信息

--- kus/Aquanju.go
+++ kus/Aquanju.go
@@ -3,12 +3,16 @@ package kus
 import (
 	"github.com/jinzhu/gorm"
 	"mhsykongzhiqi/moxings"
+	"github.com/lunny/log"
 )
 
 var Db, _ = gorm.Open("mysql", "root:root@tcp(127.0.0.1:3306)/mhsyshuju?charset=utf8&parseTime=True&loc=Local")
 
 func Chuangjianbiao() {
-	Db.Set("gorm:table_options", "ENGINE=InnoDB").CreateTable(
-		&moxings.Yinpinwenjians{},
+	db := Db.Set("gorm:table_options", "ENGINE=InnoDB").CreateTable(
+		&moxings.Yinpinxiazais{},
 	)
+	if db.Error != nil{
+		log.Println("Chuangjianbiao---db.Error",db.Error)
+	}
 }
