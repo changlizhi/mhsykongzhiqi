96793d6053395a1da647b7d6f85234ae4dc599e4
changlz <changlizhi@aliyun.com>
2018-01-12 09:46:24 +0800

    对音频密码新旧的处理

---
+++ kus/Yinpinmimajiukus.go
@@ -0,0 +1,26 @@
+package kus
+
+import (
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+	"log"
+	"mhsykongzhiqi/moxings"
+)
+
+func Charuyinpinmimajiu(moxing *moxings.Yinpinmimajius) bool {
+	cr := Db.Create(moxing)
+	if cr.Error != nil {
+		log.Println("Yinpinmimajius----cr.Error---", cr.Error)
+		return false
+	}
+	return true
+}
+func Chaxunyigeyinpinmimajiu(moxing moxings.Yinpinmimajius) *moxings.Yinpinmimajius {
+	find := Db.Find(&moxings.Yinpinmimajius{}, moxing)
+	if find.Error != nil && find.Error == gorm.ErrRecordNotFound {
+		log.Println("Chaxunyigeyinpinmimajiu--find.Error---", find.Error)
+		return nil
+	}
+	ret := find.Value.(*moxings.Yinpinmimajius)
+	return ret
+}
---
+++ kus/Yinpinmimaxinkus.go
@@ -0,0 +1,26 @@
+package kus
+
+import (
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+	"log"
+	"mhsykongzhiqi/moxings"
+)
+
+func Charuyinpinmimaxin(moxing *moxings.Yinpinmimaxins) bool {
+	cr := Db.Create(moxing)
+	if cr.Error != nil {
+		log.Println("Yinpinmimaxins----cr.Error---", cr.Error)
+		return false
+	}
+	return true
+}
+func Chaxunyigeyinpinmimaxin(moxing moxings.Yinpinmimaxins) *moxings.Yinpinmimaxins {
+	find := Db.Find(&moxings.Yinpinmimaxins{}, moxing)
+	if find.Error != nil && find.Error == gorm.ErrRecordNotFound {
+		log.Println("Chaxunyigeyinpinmimaxin--find.Error---", find.Error)
+		return nil
+	}
+	ret := find.Value.(*moxings.Yinpinmimaxins)
+	return ret
+}
