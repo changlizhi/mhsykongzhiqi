b19ef83d9d6e2ac95d3b28d3bc7dff3c8f7c7217
changlz <changlizhi@aliyun.com>
2018-01-16 23:18:07 +0800

    增加mingcheng字段，用于标记版本信息

--- moxings/Ruanjianxins.go
+++ moxings/Ruanjianxins.go
@@ -2,12 +2,13 @@ package moxings
 
 //从版本库中拿最新的版本或者拿控制器无法更新到最新版本时找一个合适的版本给控制器
 type Ruanjianxins struct {
-	Id       int
-	Xuliehao string `gorm:"not null;DEFAULT:0"`
-	Banben   string `gorm:"not null;DEFAULT:0"`
-	Weizhi   string `gorm:"not null;DEFAULT:0"`
-	Neirong  string `gorm:"not null;DEFAULT:0"`
-	Leixing  string `gorm:"not null;DEFAULT:0"`
+	Id        int
+	Xuliehao  string `gorm:"not null;DEFAULT:0"`
+	Banben    string `gorm:"not null;DEFAULT:0"`
+	Weizhi    string `gorm:"not null;DEFAULT:0"`
+	Mingcheng string `gorm:"not null;DEFAULT:0"`
+	Neirong   string `gorm:"not null;DEFAULT:0"`
+	Leixing   string `gorm:"not null;DEFAULT:0"`
 }
 
 func (Ruanjianxins) TableName() string {
