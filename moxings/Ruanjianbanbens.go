53a14b407f30bb47cc9f1737166426e1b81b2de9
changlz <changlizhi@aliyun.com>
2018-01-16 23:28:38 +0800

    fmt

--- moxings/Ruanjianbanbens.go
+++ moxings/Ruanjianbanbens.go
@@ -2,13 +2,13 @@ package moxings
 
 //用于记录所有的脚本版本及内容，防止控制器部分更新后找不到正确的版本
 type Ruanjianbanbens struct {
-	Id       int
-	Xuliehao string `gorm:"not null;DEFAULT:0"`
-	Banben   string `gorm:"not null;DEFAULT:0"`
-	Weizhi   string `gorm:"not null;DEFAULT:0"`
-	Neirong  string `gorm:"not null;DEFAULT:0"`
+	Id        int
+	Xuliehao  string `gorm:"not null;DEFAULT:0"`
+	Banben    string `gorm:"not null;DEFAULT:0"`
+	Weizhi    string `gorm:"not null;DEFAULT:0"`
+	Neirong   string `gorm:"not null;DEFAULT:0"`
 	Mingcheng string `gorm:"not null;DEFAULT:0"`
-	Leixing  string `gorm:"not null;DEFAULT:0"`
+	Leixing   string `gorm:"not null;DEFAULT:0"`
 }
 
 func (Ruanjianbanbens) TableName() string {
--- moxings/Ruanjianjius.go
+++ moxings/Ruanjianjius.go
@@ -2,13 +2,13 @@ package moxings
 
 //用于从控制器获取软件内容，做比对
 type Ruanjianjius struct {
-	Id       int
-	Xuliehao string `gorm:"not null;DEFAULT:0"`
-	Banben   string `gorm:"not null;DEFAULT:0"`
-	Weizhi   string `gorm:"not null;DEFAULT:0"`
-	Neirong  string `gorm:"not null;DEFAULT:0"`
+	Id        int
+	Xuliehao  string `gorm:"not null;DEFAULT:0"`
+	Banben    string `gorm:"not null;DEFAULT:0"`
+	Weizhi    string `gorm:"not null;DEFAULT:0"`
+	Neirong   string `gorm:"not null;DEFAULT:0"`
 	Mingcheng string `gorm:"not null;DEFAULT:0"`
-	Leixing  string `gorm:"not null;DEFAULT:0"`
+	Leixing   string `gorm:"not null;DEFAULT:0"`
 }
 
 func (Ruanjianjius) TableName() string {
